966ad44ec54163d0824438f16d9a8cbca49f37ee
LarissaBSouza <larissabandeirasouza@gmail.com>
2022-02-28 12:11:08 -0300

    primeiro commit projeto

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import Tasks from './components/Tasks'
+
+import './App.css'
+import AddTask from './components/AddTask'
+
+const App = () => {
+  //const message = 'Hello World!'
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    }
+  ])
+
+  return (
+    <>
+      <div className="container">
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
+      {/* <button onClick={() => setMessage('Hello Larissa')}> Mudar Mensagem </button>*/}
+    </>
+  )
+}
+
+export default App;
